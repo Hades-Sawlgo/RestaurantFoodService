a6c59070c9992d1b8085d2d9e506b60de62ded54
Hades <gbalyukw@gmail.com>
2022-02-03 21:17:39 -0500

    Updated Schema to Support Pizza Order Creation.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,10 @@
-DROP TABLE IF EXISTS TEST;
+DROP TABLE IF EXISTS PizzaOrder CASCADE;
 
-CREATE TABLE TEST (
-    id INTEGER,
-    primary key (id)
-);
+CREATE TABLE PizzaOrder (
+    PizzaOrderId BIGINT NOT NULL,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    PhoneNumber VARCHAR(255),
+    PizzaType VARCHAR(255),
+    PRIMARY KEY (PizzaOrderId)
+)
